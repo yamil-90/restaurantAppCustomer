00832b986b4f8230f2ccfc0f1f83407629bd8f3f
yamil-90 <yamil.coloma90@gmail.com>
2022-01-10 12:55:52 -0300

    first comit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>app</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
